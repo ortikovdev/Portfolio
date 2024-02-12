bdbd1893872254d6e71eebc9e74a4adf96165eca
anvardev <oranwhatever@gmail.com>
2024-02-12 21:19:01 +0500

    modified

--- apps/blog/urls.py
+++ apps/blog/urls.py
@@ -1,5 +1,10 @@
+from django.urls import path
+from .views import blog_view, blog_detail
+
+
 app_name = 'blog'
 
-urlpatterns = [
-
-]
+urlpatterns = {
+    path('blogslist/', blog_view, name='blog'),
+    path('bloglist<slug:slug>/', blog_detail, name='detail'),
+}
