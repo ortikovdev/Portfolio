c80010c690074eff2408538a3174fb5dcac211de
anvar <ortikovua@gmail.com>
2024-02-14 09:28:05 +0500

    modified

--- apps/blog/urls.py
+++ apps/blog/urls.py
@@ -4,13 +4,7 @@ from .views import blog_view, blog_detail
 
 app_name = 'blog'
 
-<<<<<<< HEAD
-urlpatterns = [
-
-]
-=======
 urlpatterns = {
     path('blogslist/', blog_view, name='blog'),
     path('bloglist<slug:slug>/', blog_detail, name='detail'),
 }
->>>>>>> origin/main
--- apps/resume/views.py
+++ apps/resume/views.py
@@ -1,3 +1,27 @@
 from django.shortcuts import render
+from .models import (
+    Skill,
+    Education,
+    Experience,
+    Awards,
+    Cervices,
+    OurProjects,
+)
 
-# Create your views here.
+
+def resume_page(request):
+    skills = Skill.objects.all()
+    education = Education.objects.all()
+    experience = Experience.objects.all()
+    awards = Awards.objects.all()
+    cervices = Cervices.objects.all()
+    our_projects = OurProjects.objects.all()
+    ctx = {
+        'skills': skills,
+        'education': education,
+        'experience': experience,
+        'awards': awards,
+        'cervices': cervices,
+        'our_projects': our_projects,
+    }
+    return render(request, 'main/index.html', ctx)
