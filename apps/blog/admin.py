53ca8b04429ca431c19611c7cbda19d3735d3d27
anvar <ortikovua@gmail.com>
2024-02-15 14:47:18 +0500

    modified

--- apps/blog/admin.py
+++ apps/blog/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import (
+    Category,
+    Tag,
+    Blog,
+    Comment,
+)
 
-# Register your models here.
+admin.site.register(Category)
+admin.site.register(Tag)
+admin.site.register(Blog)
+admin.site.register(Comment)
